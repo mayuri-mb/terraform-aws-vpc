9a88e5187329ee109da198981a43db1b27c77f8c
mayuri <mayu.reddy10@gmail.com>
2026-03-03 23:00:44 +1100

    actions

--- main.tf
+++ main.tf
@@ -111,9 +111,8 @@ tags = merge(local.common_tags,
 )      
 }
 
-
 resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.main.id
+  allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public[0].id
 
   tags = merge(local.common_tags,
